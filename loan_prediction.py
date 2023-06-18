abae5924d60a60dca6faf1be14e4c0109110d03e
user-demo-project <35842360+user-demo-project@users.noreply.github.com>
2023-06-19 01:16:33 +0530

    Update loan_prediction.py

--- loan_prediction.py
+++ loan_prediction.py
@@ -300,6 +300,7 @@ if authentication_status:
                print(round(probability[0],2),"hjhjh")
                ans=round(probability[0],2)
                print(type(int(ans)),ans)
+               st.text("Probability: "+str(ans))
                c1=(Gauge()
                                    .add("", [("Probability", int(ans*100))], radius="80%")
                                    .set_global_opts(title_opts=opts.TitleOpts(title="Probability"))
@@ -360,7 +361,7 @@ if authentication_status:
             
             ans=round(probability[0],2)
             print(type(int(ans)),ans)
-            st.text("Probability: "+ans)
+            st.text("Probability: "+str(ans))
             c1=(Gauge()
                                 .add("", [(".", int(ans*100))],radius="80%")
                                 .set_global_opts(title_opts=opts.TitleOpts(title="Probability"))
